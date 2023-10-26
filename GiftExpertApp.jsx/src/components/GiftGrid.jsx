c923d8ecfcbc3445c020724cd6a670465e53a818
Diego Antonio Angel Rodriguez <2122300432@soy.utj.edu.mx>
2023-10-25 19:06:08 -0600

    Complete the funtionality of the Gift page

--- GiftExpertApp.jsx/src/components/GiftGrid.jsx
+++ GiftExpertApp.jsx/src/components/GiftGrid.jsx
@@ -1,15 +1,32 @@
 import { getGifs } from "../helpers/getGifs"
 import { useState } from "react";
 import { useEffect } from "react";
+import { GiftItem } from "./GiftItem";
 
 export const GiftGrid = ({category}) => {
+  const[images, setImages] = useState([]);
+  const getImages = async() => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  }
+
   useEffect(() => {
-    getGifs(category);    
+    getImages();    
   }, [])
 
   return(
     <>
-      <h3>{category}</h3>      
+      <h1>{category}</h1>
+      <div className="card-grid">
+        {
+          images.map((image) => (
+            <GiftItem 
+              key={image.id}
+              {...image}
+            />
+          ))
+        }  
+      </div>      
     </>
   )
 }
